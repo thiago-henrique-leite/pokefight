36461a65d8a6a39a4013dcfc31c26a73729c9acf
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-08-20 17:09:36 -0300

    feat: add get pokemon request in pokeapi

---
+++ lib/pokefight/api/pokemon.ex
@@ -0,0 +1,34 @@
+defmodule Pokefight.Api.Pokemon do
+  @pokeapi_host Application.get_env(:pokefight, :pokeapi_host)
+
+  require Logger
+
+  def get_pokemon(pokename) do
+    get("pokemon/#{pokename}")
+  end
+
+  def get(endpoint) do
+    process_response(HTTPoison.get("#{@pokeapi_host}#{endpoint}"))
+  end
+
+  defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, Poison.decode!(body)}
+  end
+
+  defp process_response({:ok, %HTTPoison.Response{status_code: status_code, body: body}}) do
+    Logger.info("PokeApi response error: #{body}")
+
+    case status_code do
+      400 -> {:error, "bad_request"}
+      401 -> {:error, "unauthorized"}
+      403 -> {:error, "forbidden"}
+      404 -> {:error, "not_found"}
+      500 -> {:error, "internal_server_error"}
+      _ -> {:error, "unexpected_error"}
+    end
+  end
+
+  defp process_response({:error, _}) do
+    {:error, "PokeApi integration problem"}
+  end
+end
